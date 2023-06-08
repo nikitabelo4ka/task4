3988b3d20d4681e374b99af1a7ed65ac9100cdeb
Nikita <belko.nikita@yandex.ru>
2023-06-08 20:26:34 +0300

    Working !

---
+++ task4.java
@@ -0,0 +1,7 @@
+//\u000A\u002F\u002A
+console.log("Itransition");//\u002A\u002F\u000A
+//\u000Apublic class task4{
+    //\u000Apublic static void main(String[] args){
+        //\u000ASystem.out.println("Itransition");
+    //\u000A}
+//\u000A}
